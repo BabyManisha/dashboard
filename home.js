e6529036e3e59f8667d73d7084ea623e7c39cfd6
BabyManisha <babymaneesha@gmail.com>
2017-01-21 22:16:22 +0530

    url21

--- home.js
+++ home.js
@@ -27,9 +27,7 @@ $(document).ready(function () {
   	    	      FB.getLoginStatus(function(response) {
   	    	            if (response.status === 'connected') {
   	    	                  // displayImage();
-  	    	                  console.log("SMSMSMSMSMSMSMSMSMSMSMSMMS");
   	    	                  console.log(response);
-  	    	                  console.log("SMSMSMSMSMSMSMSMSMSMSMSMMS");
   	    	                  // sdbdfn
   	    	                  self.fbstatus = true;
   	    	                  self.accessToken = response.authResponse.accessToken;
@@ -40,9 +38,7 @@ $(document).ready(function () {
   	    	                  FB.login(function(response) {
   	    	                        if (response.authResponse) {
   	    	                                    // displayImage();
-  	    	                                    console.log("SMSMSMSMSMSMSMSMSMSMSMSMMS");
   	    	                                    console.log(response);
-  	    	                                    console.log("SMSMSMSMSMSMSMSMSMSMSMSMMS");
   	    	                                    self.fbstatus = true;
   	    	                                    self.accessToken = response.authResponse.accessToken;
   	    					  					self.userID = response.authResponse.userID;
@@ -72,7 +68,7 @@ $(document).ready(function () {
   	    methods: {
   	    	redirect (){
   	    		var self = this;
-  	    		var fbapi = "https://comb.shivaprasanth.info/api/love?accessToken="+self.accessToken+"&userID="+self.userID;
+  	    		var fbapi = "http://comb.shivaprasanth.info/api/love?accessToken="+self.accessToken+"&userID="+self.userID;
   	    		  $.getJSON(fbapi)
   	    		    .done(function( json ) {
   	    		      console.log(json);
@@ -130,17 +126,17 @@ $(document).ready(function () {
 	    	var googleNews = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=dfa97dc15cea425d9fbc3829ef7582f1";
 	    	  $.getJSON( googleNews )
 	    	    .done(function( json ) {
-	    	      console.log( "JSON Data: " + json['articles'][0] );
+	    	      // console.log( "JSON Data: " + json['articles'][0] );
 	    	      for(let ob in json['articles']){
 	    	      	var news = json['articles'][ob];
 	    	      	for(let ne in news){
-	    	        	console.log( "JSON Data: " + news['description'] );
+	    	        	// console.log( "JSON Data: " + news['description'] );
 	    	        	if(self.headlines.indexOf(news['description']) == -1 && self.headlines.length <= 1){
 	    	        		self.headlines.push(news['description']);
 	    	        	}
 	    	      	}
 	    	      }
-	    	      console.log(self.headlines);
+	    	      // console.log(self.headlines);
 	    	    })
 	    	    .fail(function( jqxhr, textStatus, error ) {
 	    	      var err = textStatus + ", " + error;
@@ -150,12 +146,12 @@ $(document).ready(function () {
 	    	var youtubeVideo = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&chart=mostPopular&regionCode=IN&maxResults=1&key=AIzaSyCpeVeLJb9W0bKiz4s07GagM9hU9Jjr138";
 	    	$.getJSON( youtubeVideo )
 	    	  .done(function( json ) {
-	    	    console.log( "JSON Data: " + json['items'][0]['id'] );
+	    	    // console.log( "JSON Data: " + json['items'][0]['id'] );
 	    	    // for(let ob in json['items']){
 	    	    // 	var news = json['items'][ob];
 	    	    // }
 	    	    self.video = json['items'][0]['id'];
-	    	    console.log(self.headlines);
+	    	    // console.log(self.headlines);
 	    	  })
 	    	  .fail(function( jqxhr, textStatus, error ) {
 	    	    var err = textStatus + ", " + error;
